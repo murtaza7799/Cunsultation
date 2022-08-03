23a97aab1d6ff51ea85569ff6a5cd628fdc39515
Muhammad Murtaza <48488384+murtaza7799@users.noreply.github.com>
2022-08-04 02:37:23 +0500

    change environment

--- util/check-environment.ts
+++ util/check-environment.ts
@@ -1,8 +1,15 @@
+import { useRouter } from 'next/router';
 export default function checkEnvironment(): string {
-  const envUrl =
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3000'
-      : 'https://trello-clone-one.vercel.app';
+  function getURL() {
+    const { href } = window.location;
+    const origin = getLocationOrigin();
+    return href.substring(origin.length);
+  }
+  function getLocationOrigin() {
+    const { protocol, hostname, port } = window.location;
+    return `${protocol}//${hostname}${port ? ':' + port : ''}`;
+  }
+  const envUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : getURL();
 
   return envUrl;
 }
