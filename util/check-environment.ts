5afb2dedfa26af685b399c05b6394e70a7ea4019
Muhammad Murtaza <48488384+murtaza7799@users.noreply.github.com>
2022-08-04 03:32:55 +0500

    env url

--- util/check-environment.ts
+++ util/check-environment.ts
@@ -1,6 +1,8 @@
 export default function checkEnvironment(): string {
   const envUrl =
-    process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : process.env.WEB_URL;
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:3000'
+      : 'https://cunsultation-hlyvyp88r-murtaza7799.vercel.app/';
 
   return envUrl;
 }
