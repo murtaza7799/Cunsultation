635c1e84109e2e5fb8b3ef0865ef4c6ff339d775
Muhammad Murtaza <48488384+murtaza7799@users.noreply.github.com>
2022-08-07 17:49:05 +0500

    remove

--- util/mongodb.js
+++ util/mongodb.js
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb';
+// import { MongoClient } from 'mongodb';
 
 const { MONGODB_URI, MONGODB_DB } = process.env;
 
@@ -14,34 +14,34 @@ if (!MONGODB_DB) {
  * Global is used here to maintain a cached connection across hot reloads
  * in development. This prevents connections growing exponentially
  * during API Route usage.
- */
-let cached = global.mongo;
+//  */
+// let cached = global.mongo;
 
-if (!cached) {
-  cached = global.mongo = { conn: null, promise: null };
-}
+// if (!cached) {
+//   cached = global.mongo = { conn: null, promise: null };
+// }
 
-export async function connectToDatabase() {
-  if (cached.conn) {
-    return cached.conn;
-  }
+// export async function connectToDatabase() {
+//   if (cached.conn) {
+//     return cached.conn;
+//   }
 
-  if (!cached.promise) {
-    const opts = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    };
+//   if (!cached.promise) {
+//     const opts = {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true
+//     };
 
-    const mongoURL =
-      process.env.NODE_ENV === 'development' ? process.env.LOCAL_MONGODB : process.env.MONGODB_URI;
+//     const mongoURL =
+//       process.env.NODE_ENV === 'development' ? process.env.LOCAL_MONGODB : process.env.MONGODB_URI;
 
-    cached.promise = MongoClient.connect(mongoURL, opts).then((client) => {
-      return {
-        client,
-        db: client.db(MONGODB_DB)
-      };
-    });
-  }
-  cached.conn = await cached.promise;
-  return cached.conn;
-}
+//     cached.promise = MongoClient.connect(mongoURL, opts).then((client) => {
+//       return {
+//         client,
+//         db: client.db(MONGODB_DB)
+//       };
+//     });
+// }
+// cached.conn = await cached.promise;
+// return cached.conn;
+// }
