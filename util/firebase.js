6e7d144337466628ee6912c83c52839c4f352202
Muhammad Murtaza <48488384+murtaza7799@users.noreply.github.com>
2023-08-23 09:02:12 -0600

    Update firebase.js

--- util/firebase.js
+++ util/firebase.js
@@ -10,13 +10,14 @@ import { getStorage } from 'firebase/storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyCL3a4pqYF0b4SI7v3oSwTcxvWLkw-O7Z4',
-  authDomain: 'rooks-360015.firebaseapp.com',
-  projectId: 'rooks-360015',
-  storageBucket: 'rooks-360015.appspot.com',
-  messagingSenderId: '550849078903',
-  appId: '1:550849078903:web:498020646e1a82fb2a4872',
-  measurementId: 'G-X7WKD94QR8'
+  apiKey: "AIzaSyDnR4jVplbcDuwkVW8mRptyRP42Z1vfxPA",
+  authDomain: "clientproject-6aeff.firebaseapp.com",
+  databaseURL: "https://clientproject-6aeff-default-rtdb.firebaseio.com",
+  projectId: "clientproject-6aeff",
+  storageBucket: "clientproject-6aeff.appspot.com",
+  messagingSenderId: "908410256076",
+  appId: "1:908410256076:web:810ff56f02974656c36069",
+  measurementId: "G-7NXX8ELP86"
 };
 
 initializeApp(firebaseConfig);
